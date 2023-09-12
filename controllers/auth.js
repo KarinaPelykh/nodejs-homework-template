b7676d740f9bf23c8511ef4aadf0812dc1383f99
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2023-09-12 09:51:11 +0200

    Add: updateAvatars, setting add foto,gravata

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,17 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-
+const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
 const { HttpError } = require("../helpers");
 
 const { cntrWrapper } = require("../decorator");
 
 const { JWT_SECRET } = process.env;
+
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 const signUp = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
@@ -14,8 +19,14 @@ const signUp = async (req, res) => {
     throw HttpError(409, "Email in use");
   }
 
+  const avatarURL = gravatar.url(email);
+
   const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
   res.status(201).json({
     email: newUser.email,
     subscription: newUser.subscription,
@@ -70,10 +81,30 @@ const updateSubscription = async (req, res) => {
 
   res.status(200).json(result);
 };
+
+const updataAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tmpUpload, originalname } = req.file;
+  const filename = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, filename);
+
+  const avatarSize = await Jimp.read(tmpUpload);
+  avatarSize.resize(250, 250);
+
+  await fs.rename(tmpUpload, resultUpload);
+  const avatarURL = path.join("avatar", filename);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({
+    avatarURL,
+  });
+};
+
 module.exports = {
   signUp: cntrWrapper(signUp),
   signIn: cntrWrapper(signIn),
   getCurrent: cntrWrapper(getCurrent),
   signOut: cntrWrapper(signOut),
   updateSubscription: cntrWrapper(updateSubscription),
+  updataAvatar: cntrWrapper(updataAvatar),
 };
